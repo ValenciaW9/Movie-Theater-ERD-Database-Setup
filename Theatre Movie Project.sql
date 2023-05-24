b732c57f92cba380a9772ef0c4dccdd29264a648
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-05-23 23:00:36 -0400

    Movie Theater ERD + Database Setup
    Movie Theater ERD + Database Setup

---
+++ Theatre Movie Project.sql
@@ -0,0 +1,32 @@
+Movie Theater Premire Movie Theatre ERD
+CREATE TABLE table_A( Customer , user_id
+column1 datatype, User_id varchar(50)
+column2 datatype,passwprd_hint varchar(50)
+column3 datatype, emailvarchar(25)
+timestamp
+MediumINT(size)Boolean
+...
+CREATE TABLE table_b PERSONS (
+PersonID Int, selller_id, varchar(50)
+LASTName varchar (255),
+FirstName varchar (255),
+Address varchar  (255),
+City varchar(255)
+Category_ID
+);
+CREATE TABLE new_table_c 
+SELECT column COLUMN 2,...
+FROM existing_table_name
+WHERE...;
+Seller_id
+SELECT Category_id
+Company_ID
+CREATE TABLE Product varchar(100)
+WHERE Money, Time, Currency(50)
+SELECT product_skuorders.product.sku(50) , Ticket Currency ; Date AND time 
+FROM company TO customer one ticket per Jordan Air OR Marvel Film ONLY one theatre per person Boolean );
+
+
+
+
+
